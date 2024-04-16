2f3dbeb09c86a8035ff57a325c7fe70a4569d7d8
whitney shisia <shisiawhitney215@gmail.com>
2024-04-16 17:53:39 +0300

    index,js

--- src/index.js
+++ src/index.js
@@ -13,3 +13,33 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+// Fetch Posts
+fetch("http://localhost:3000/toys")
+  .then((response) => response.json())
+  .then((toys) => {
+    displayToys(toys);
+  })
+  .catch((error) => {
+    console.error('Error fetching toys:', error);
+  });
+
+function displayToys(toys) {
+  let cardsContainer = document.getElementById("toy-collection");
+  cardsContainer.innerHTML = ""; // Clear previous content
+  
+  toys.forEach((toy) => {
+    let { id, name, image,likes } = toy; // Assuming toy object has id, name, and image properties
+    cardsContainer.innerHTML += `
+      <div class="toy-collection" >
+        <div class="card">
+          <h2>${name}</h2>
+          <img src="${image}" class="toy-avatar" />
+          <p>${likes} likes</p>
+          <button class="like-btn" id="${id}">Like  ❤️</button>
+          </div>
+        </div>
+      </div>
+    `;
+  });
+}
